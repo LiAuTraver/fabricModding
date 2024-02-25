8593b47d42ceffc688dbaee2e4e72488e9c9ce21
LiAuTraver <63725840+LiAuTraver@users.noreply.github.com>
2024-02-25 23:39:10 +0800

    Add sound

--- src/main/java/net/ancillarycat/fabricfirststeps/FabricFirstSteps.java
+++ src/main/java/net/ancillarycat/fabricfirststeps/FabricFirstSteps.java
@@ -1,32 +1,37 @@
 package net.ancillarycat.fabricfirststeps;
 
+import net.ancillarycat.item.MyFirstItem;
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import net.ancillarycat.item.MyFirstItem;
+
 public class FabricFirstSteps implements ModInitializer {
-	public static final String MOD_ID = "fabricfirststeps";
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final String MOD_ID = "fabricfirststeps";
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Item MY_FIRST_ITEM = Registry.register(Registries.ITEM, new Identifier(FabricFirstSteps.MOD_ID, "my_first_item"),new MyFirstItem(new FabricItemSettings()));
 
-	// my first item instance
-//	public static final Item myFirstItem = Registry.register(Registries.ITEM, new Identifier("fabric first mod", "myFirstItem"), new Item(new FabricItemSettings()));
+//	private static void addItemToIngredientTable(FabricItemGroupEntries entries) {
+//		entries.add(MY_FIRST_ITEM);
+//	}
 
-	@Override
-	public void onInitialize(){
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    public static void loggerInfo() {
+        FabricFirstSteps.LOGGER.info("Registering items for my first mod" + FabricFirstSteps.MOD_ID);
+//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToIngredientTable);
+    }
 
-		LOGGER.info("Hello Fabric world!");
-		MyFirstItem.registerModItems();
-
-	}
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        loggerInfo();
+    }
 }
--- src/main/java/net/ancillarycat/item/MyFirstItem.java
+++ src/main/java/net/ancillarycat/item/MyFirstItem.java
@@ -22,29 +22,10 @@ public class MyFirstItem extends Item {
     public MyFirstItem(Settings settings) {
         super(settings);
     }
-
-    public static final String MOD_ID = "fabricfirststeps";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
-        LOGGER.info("successfully applied sound");
+        FabricFirstSteps.LOGGER.info("successfully applied sound");
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
-    }
-
-    public static final Item MY_FIRST_ITEM = registerItem("my_first_item", new Item(new FabricItemSettings()));
-
-    private static void addItemToIngredientTable(FabricItemGroupEntries entries) {
-        entries.add(MY_FIRST_ITEM);
-    }
-
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(FabricFirstSteps.MOD_ID, name), item);
-    }
-
-    public static void registerModItems() {
-        FabricFirstSteps.LOGGER.info("Registering items for my first mod" + FabricFirstSteps.MOD_ID);
-//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToIngredientTable);
     }
 }
