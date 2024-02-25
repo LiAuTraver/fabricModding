ed7c989a108717862ec71936918e164dfb076b3f
LiAuTraver <63725840+LiAuTraver@users.noreply.github.com>
2024-02-25 15:16:44 +0800

    initial setup

---
+++ src/main/java/net/ancillarycat/fabricfirststeps/FabricFirstSteps.java
@@ -0,0 +1,31 @@
+package net.ancillarycat.fabricfirststeps;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.rmi.registry.Registry;
+
+public class FabricFirstSteps implements ModInitializer {
+	public static final String MOD_ID = "fabricfirststeps";
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// my first item instance
+	public static final Item myFirstItem = Registry.register(Registries.ITEM, new Identifier("fabric first mod", "myFirstItem"), new Item(new FabricItemSettings()));
+
+	@Override
+	public void onInitialize(){
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
