4113d8b993c8fe4e5dc2b8f9a0509d9fe8ac07c6
LiAuTraver <63725840+LiAuTraver@users.noreply.github.com>
2024-02-28 00:04:03 +0800

    add block state(failed)

--- src/main/java/net/ancillarycat/fabricfirststeps/MyFirstBlock.java
+++ src/main/java/net/ancillarycat/fabricfirststeps/MyFirstBlock.java
@@ -14,21 +14,26 @@ import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class MyFirstBlock  extends Block {
-    public MyFirstBlock(Settings settings){
+public class MyFirstBlock extends Block {
+    public MyFirstBlock(Settings settings) {
         super(settings);
     }
 
     @Override
-    public ActionResult onUse(BlockState blockState, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
-        if(!world.isClient){
-            player.sendMessage(Text.literal("successfully applied onUse function"),false);
+    public ActionResult onUse(BlockState blockState, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(Text.literal("successfully applied onUse function"), false);
         }
         return ActionResult.SUCCESS;
     }
 
     @Override
-    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context){
-        return VoxelShapes.cuboid(0f,0f,0f,0f,1.0f,0.5f);
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 0.5f);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return this.collidable ? state.getOutlineShape(world, pos) : VoxelShapes.empty();
     }
 }
